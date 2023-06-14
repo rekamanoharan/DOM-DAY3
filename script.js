3f353d93e3ac60778ab50bc54ef15b7551b1d4ab
rekamanoharan <92388358+rekamanoharan@users.noreply.github.com>
2023-06-14 10:39:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
-buttons.forEach(index =>{
-        index.addEventListener("click",()=>{
-            var num = index.innerHTML;
-        if(num == "="){
-            var sum = eval(input.value);
-            input.value = sum;
-        } else if(num == "AC") {
-            input.value = "";
-        } else if(num == "DEL"){
-            var value = input.value;
-            value = value.substr(0,value.length-1);
-            input.value = value;
-        } else{
-            input.value += num;
-        }
-    });
-});
-document.addEventListener("keydown",function(e){
-    if(e.which == 13){
-        var sum = eval(input.value);
-        input.value = sum;
-    } else if(e.which == 27){
-        input.value = "";
-    } else if(e.code.indexOf("Digit") != -1 || e.code.indexOf("Numpad") != -1 || e.code.indexOf("Minus") != -1){
-        var myKey = e.key;
-        input.value += myKey;
-    } else if(e.code.indexOf("Backspace") != -1){
-        var value = input.value;
-        value = value.substr(0,value.length-1);
-        input.value = value;
-    } else {
-        return alert("only numbers");
+let result=document.getElementById("result")
+
+let calculate=(number) => {
+     result.value+=number;
+ }
+
+let Result=()=>{
+   try{
+        result.value=eval(result.value)
     }
+    catch(err){
+        alert("Invalid input");
+    }
+}
+
+ function clr(){
+    result.value=""
+ }
+ function del(){
+    result.value=result.value.slice(0,-1)
+ }
+ 
+ document.addEventListener("keydown",function(e){
+    e.preventDefault()
+    if(e.code.indexOf("Digit") != -1 ||  e.code.indexOf("Numpad") != -1 || e.code.indexOf("Minus") != -1){
+        var myKey = e.key;
+        result.value += myKey;
+    } 
+    else {
+        // result.value=""
+        return alert("Only Numbers are allowed");
+    }
+
 });
